f02cb7512e594b92e914a8619218ec3ff5133fec
John Oberlin <oberljn@gmail.com>
2020-01-18 19:34:49 -0500

    Update url_status_bulk_check.py

--- url_status_bulk_check.py
+++ url_status_bulk_check.py
@@ -1,31 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[68]:
-
-
 import requests as req
 from datetime import datetime
 import time
-
-
-# In[57]:
-
 
 # Paste links column within t
 t = """www.example.com
 www.example.com/anotherLink"""
 
-
-# In[58]:
-
-
 # Get links into a list
 links = t.split("\n")
-
-
-# In[71]:
-
 
 # Preview links list
 for i in links[:5]:
@@ -34,10 +16,6 @@ for i in links[:5]:
 sec = 10
 print("Paused for {} seconds.".format(str(sec)))
 time.sleep(sec)
-
-
-# In[60]:
-
 
 # Get link status code, build CSV for download
 csv = ""
@@ -48,10 +26,6 @@ for link in links:
     except:
         csv += link + "," + "JO's script error" + "\n"
 
-
-# In[61]:
-
-
 # Preview CSV output
 preview = csv.split("\n")
 for i in preview[:5]:
@@ -60,15 +34,8 @@ for i in preview[:5]:
 print("Paused for {} seconds.".format(str(sec)))
 time.sleep(sec)
 
-
-# In[62]:
-
-
 # Write out to CSV file (saves to machine)
 now = datetime.now()
 timestamp = now.strftime("%Y-%m-%d_%H%M")
 with open("link_status_" + timestamp + ".csv", "w") as f:
     f.write(csv)
-
-
-# In[ ]:
